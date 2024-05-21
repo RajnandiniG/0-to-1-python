06a8799708074038bcbe65207477acb0dc5bc75d
RajnandiniG <160368396+RajnandiniG@users.noreply.github.com>
2024-05-21 18:52:22 -0400

    Update 1. Addition.py

--- 1. Basics/1. Addition.py
+++ 1. Basics/1. Addition.py
@@ -2,3 +2,15 @@
 a = input('First number: ')
 b = input('Second number: ')
 print('Total:', float(a) + float(b))
+
+#print **
+i=1
+while i<=5:
+    print(i*'*')
+    i=i+1
+
+#print no's
+i=1
+while i<=5:
+    print(i)
+    i=i+1
