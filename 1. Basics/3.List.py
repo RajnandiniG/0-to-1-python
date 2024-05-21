56f1e2405cdd8d46fafc43d98e886d4ee1b9a25e
RajnandiniG <160368396+RajnandiniG@users.noreply.github.com>
2024-05-21 19:35:03 -0400

    Update and rename 3.DS.py to 3.List.py

--- 1. Basics/3.List.py
+++ 1. Basics/3.List.py
@@ -1,4 +1,4 @@
-#list
+#Play with list
 mylist = [0, 1, "two", 3.2, False]
 mytuple = (0, "eighteen", 2)
 mydict = {"one" : 1, "two" : 11}
@@ -11,3 +11,10 @@ mylist.insert(3, 6) # insert(index,value) if you want to add an element at a spe
 print(mylist)
 print(mylist[0:6:2]) # use slices to get parts of a sequence
 print(mylist[::-1]) # you can use slices to reverse a sequence
+
+print(3.2 in mylist) # in is used to find value in list
+print("Number of elements in list:",str(len(mylist))) # to count list elements
+mylist.remove(7) # need to pass value, expects one arg
+print(mylist)
+mylist.clear() # to empty the list, no arg required
+print(mylist)
