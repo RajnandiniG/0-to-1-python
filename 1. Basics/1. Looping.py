bf7a4acb4a7100a55a856008fe05312757520e61
RajnandiniG <160368396+RajnandiniG@users.noreply.github.com>
2024-05-21 22:37:01 -0400

    Update and rename 1. Addition.py to 1. Looping.py

---
+++ 1. Basics/1. Looping.py
@@ -0,0 +1,26 @@
+#print **
+i=1
+while i<=5:
+    print(i*'*')
+    i=i+1
+
+#print no's
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+    
+#for loop
+num = [1,2,3,4,5,11,22]
+for i in num:
+    print(i)
+
+#for is better than while
+i=0
+while i<len(num):
+    print(num[i])
+    i=i+1
+
+#range
+for i in range(5):
+    print(i+1)
