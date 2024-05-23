03f2e68dcab9df7ab0aa11d6766099f6b3522b3f
RajnandiniG <160368396+RajnandiniG@users.noreply.github.com>
2024-05-23 17:38:16 -0400

    Update 4.Strings.py

--- 1. Basics/4.Strings.py
+++ 1. Basics/4.Strings.py
@@ -14,5 +14,15 @@ print(course[1:11]) #it skips last index
 name='Thomas Peterson'
 print(name[1:-3]) #by default will consider -1 last index
 
+#Fuctions with String
+course='Python is awesome to learn!'
+print(len(course)) #To know the length of string
+print(course.upper()) #Convert in uppercase
+print(course.lower()) #Convert in lowercase
+print(course.find('a')) #Returns index number
+print(course.find('z')) #Returns -1 if not found
+print(course.replace("Python","Go")) #replace the word
+print(course.replace("a","M")) #Replace index value, can't use index no to replace
+
 age=int(input('Enter your age:'))
 print(f'Hi there, you are {age} years old!') #f-string, which is a way to format strings in Python
