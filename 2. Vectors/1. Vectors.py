bd67ca4ce44bc31f3b0fed6c3a8dc665b42112f1
RajnandiniG <160368396+RajnandiniG@users.noreply.github.com>
2024-05-22 21:51:12 -0400

    Create 1. Vectors.py

---
+++ 2. Vectors/1. Vectors.py
@@ -0,0 +1,14 @@
+#Install numpy library to work with Vectors
+#pip install numpy
+
+#Run this basic python program to get familiar with vectors
+import numpy as np
+#Orientation-less simple array
+VectorAsArray = np.array([1,2,3,4,5])
+#Row vector
+rowAsArray = np.array([[1,2,3,4,5]])
+#Column vector
+columnAsArray=np.array([[1],[2],[3],[4],[5]])
+print(VectorAsArray)
+print(rowAsArray)
+print(columnAsArray)
