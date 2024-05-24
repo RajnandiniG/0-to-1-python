d52c3cb147f7c24de193265804503f80581615aa
RajnandiniG <160368396+RajnandiniG@users.noreply.github.com>
2024-05-24 12:59:50 -0400

    Update 5. Calendar.py

--- 1. Basics/5. Calendar.py
+++ 1. Basics/5. Calendar.py
@@ -1,3 +1,8 @@
 import calendar
 text_cal=calendar.TextCalendar()
+#Print calendar
 text_cal.prmonth(2026,12)
+
+#Display calendar in shell type...
+#$ python -m calendar
+#$ python -m calendar 2054 12
